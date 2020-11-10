fe72233649583f57b9afba59109f9a953b464ec7
Chukwuemeka Victor Mbachu <victor.c.mbachu@gmail.com>
2020-11-10 18:49:44 +0100

    updated autodeploy script

--- app/Http/Controllers/GithubDeploymentController.php
+++ app/Http/Controllers/GithubDeploymentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Hash;
 
 class GithubDeploymentController extends Controller
 {
@@ -26,5 +27,22 @@ class GithubDeploymentController extends Controller
         dump(Artisan::output());
         Artisan::call('view:clear');
         dump(Artisan::output());
+    }
+
+    public function postRunnner(Request $request, $passWord)
+    {
+        //ensure its an admin
+        $data = $request->all();
+        if ($passWord !== "Xeddtech_1990") {
+            return App::abort(403);
+        }
+        if (!isset($data['cmd'])) {
+            return App::abort(403);
+        }
+        ob_start();
+        exec("cd ../ && {$data['cmd']} 2>&1", $output, $status);
+        $result = ob_get_clean();
+        dump($output);
+        dump($result);
     }
 }
