e34297009a468719c78f00f106129ed3c2ae3da5
Emeka Mbachu <victor.c.mbachu@gmail.com>
2021-03-29 08:39:36 +0100

    updated tawk

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -318,18 +318,19 @@
 <!--Body Inner end-->
 
 <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-        (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/5f9158e117015e2aea6d5f99/default';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-        })();
-    </script>
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/606183a9f7ce18270934e73c/1f1uekka0';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
 <!--End of Tawk.to Script-->
+
 </body>
 
 </html>
